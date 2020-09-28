09b6bb21401a6174e378846ab281ba12a2a859b7
DnvSaiteja <dnvsaiteja@gmail.com>
2020-09-28 19:23:10 +0530

    Adding Route guards to servers and it child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth-guard.service';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
@@ -12,7 +13,11 @@ const appRoutes:Routes=[
     {'path':'', component:HomeComponent},
   {'path':'users',component:UsersComponent,children:[{'path': ':id/:name',component:UserComponent}]},
   
-  {'path': 'servers', component: ServersComponent,children:[{'path':':id',component:ServerComponent},{'path':':id/edit',component:EditServerComponent},
+  {'path': 'servers',
+//   canActivate:[AuthGuard] 
+canActivateChild:[AuthGuard]
+  ,component: ServersComponent,
+  children:[{'path':':id',component:ServerComponent},{'path':':id/edit',component:EditServerComponent},
   
   ]},
   {'path':'not-found',component:PageNotFoundComponent},
