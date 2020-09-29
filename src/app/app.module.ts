1034bd3dfc1d7641bf67f6778ee9cf543fbd1901
DnvSaiteja <dnvsaiteja@gmail.com>
2020-09-29 13:15:40 +0530

    End of routing section and added Resolve service for the servers component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivate.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 
 
@@ -30,7 +32,8 @@ import { CanDeactivateGuard } from './servers/edit-server/can-deactivate.service
     UserComponent,
     EditServerComponent,
     ServerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +42,7 @@ import { CanDeactivateGuard } from './servers/edit-server/can-deactivate.service
     
 
   ],
-  providers: [ServersService,AuthGuard,AuthService,CanDeactivateGuard],
+  providers: [ServersService,AuthGuard,AuthService,CanDeactivateGuard,ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
